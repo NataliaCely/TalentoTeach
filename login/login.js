d30a744daa5cd5964e90e639f3999e9e9e215a9a
NataliaCely <nataliacely9819@gmail.com>
2024-08-08 21:28:53 -0500

    first commit

---
+++ login/login.js
@@ -0,0 +1,20 @@
+function login()
+{
+let user = document.getElementById("user").value;
+let clave = document.getElementById("pw").value;
+
+if(user == "" || clave == ""){
+
+    alert("Los datos no pueden ser vacios");
+    
+}
+
+
+    else if(user=="Carlos" && clave=="1234"){
+            window.location="index.html";
+    }
+        else{
+            alert("Datos Incorrecotos");
+            window.location="login.html";
+        }
+}
